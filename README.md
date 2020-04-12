ad6a56005938220b9fc3ccc6208d0940b5cfdf36
Muhammad Albayati <16807405+malbayati1@users.noreply.github.com>
2020-04-12 11:16:40 -0700

    adding jquery

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # personal-website
+
+This is my personal portfiolo website. If you want to use my code, get in touch with me.
---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#construction").click(function() {
+        $(this).hide();
+    });
+});
