1ce875d6c094a556bf50a5e2118f8f8aab9e2ce0
unknown <16807405+malbayati1@users.noreply.github.com>
2020-06-21 17:35:40 -0700

    finished contact form

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
     $("#construction").click(function() {
         $(this).hide();
     });
+
+    // This is for the second iteration of the website
+    // $('#submitButton').click(function(event) {
+    //     event.preventDefault()
+
+    //     let email = $("#emailInput").val();
+    //     let subject = $("#subject").val();
+    //     let message = $("#message").val();
+    //     // let statusElement = $('#status');
+    //     // statusElement.empty();
+
+    //     if(email.length > 5 && email.includes('@') && email.includes('.')) {
+
+    //     } else {
+    //         // statusElement.append("<div>Please enter a vaild email address</div>");
+    //         alert("Please enter a valid email address");aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    //     }
+    // })
 });
