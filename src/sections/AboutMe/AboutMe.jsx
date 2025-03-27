71498624fba84da4025f3a6651ed693c4aa3f400
unknown <16807405+malbayati1@users.noreply.github.com>
2025-03-26 21:36:31 -0700

    Removed resume button

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -6,7 +6,6 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function AboutMe() {
@@ -47,9 +46,6 @@ function AboutMe() {
         <p className={styles.description}>
           Results driven developer looking forward to working with you on your next big project.
         </p>
-        <a href={CV} download>
-          <button className="hover">Resume</button>
-        </a>
       </div>
     </section>
   );
